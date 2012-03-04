aae21a9c8cd44bc85c1ac20a0080a2b96aa10186
Alex Le <alex@wordnik.com>
2012-03-03 16:29:09 -0800

    - add rails app

---
+++ imotional/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imotional::Application.config.secret_token = '679758a8f052a9c45cb4a997f7751327b69d5f7a48f429ab80be26427e38b10121b22b497f718d4567b7e93a68c2afae031a8197490ad000828805146d5a08c2'
